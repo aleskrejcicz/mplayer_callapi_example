49c4b13e1cf42da6941a064aef3a54a90e2edd12
Aleš Krejčí <aleskrejcicz@gmail.com>
2017-03-05 23:32:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,12 +33,6 @@ setup(
 	# https://packaging.python.org/en/latest/distributing.html#classifiers
 	classifiers=[
 		'Operating System :: POSIX :: Linux',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.2',
-		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python',
 	],
 )
